8124ddc7bd2b11674b53d1fa2b128ff0c3595a61
Jaseem <jaseemkpj@gmail.com>
2022-01-08 17:19:46 +0530

    sidebar changes

--- myProj/src/app/header/header.component.ts
+++ myProj/src/app/header/header.component.ts
@@ -8,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   isSidebarOpen = false;
+  isUserInfoOpen = false;
+  isCarrierSelectionOpen = false;
+  isPgOpen = false;
+  isUserPreferenceOpen = false;
+  isLogOutOpen = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleSidebar(val: boolean) {
+    this.isSidebarOpen = val;
+  }
+
+  toggleChevron(val: boolean[]) {
+    if (val[0]) {
+      this.isUserInfoOpen = !this.isUserInfoOpen;
+    } else if (val[1]) {
+      this.isCarrierSelectionOpen = !this.isCarrierSelectionOpen;
+    } else if (val[2]) {
+      this.isPgOpen = !this.isPgOpen;
+    } else if (val[3]) {
+      this.isUserPreferenceOpen = !this.isUserPreferenceOpen;
+    } else {
+      this.isLogOutOpen = !this.isLogOutOpen;
+    }
   }
 
 }
